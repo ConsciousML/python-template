53ea9b24b3988c16271844ec13a81d783b140003
Axel Mendoza <axel.mendoza@epita.fr>
2023-02-20 15:43:45 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""Installs a custom package in your Python Environment"""
+import os
+from setuptools import setup
+
+
+def read(fname: str) -> str:
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding="utf-8").read()
+
+
+setup(
+    name="ExampleCode",
+    version="0.0.1",
+    author="Axel Mendoza",
+    author_email="axel.mendoza@epita.fr",
+    description="Example description",
+    license="BSD",
+    packages=['examplecode'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
