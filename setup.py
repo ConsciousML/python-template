f91c4fd37ddbf4b5d0a7775f0e555f571bc8072c
Axel Mendoza <axel.mendoza@epita.fr>
2024-06-02 10:18:30 +0200

    change package name to template_package
    add poetry setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 """Installs a custom package in your Python Environment"""
+
 import os
 from setuptools import setup
 
@@ -8,13 +9,13 @@ def read(fname: str) -> str:
 
 
 setup(
-    name="Dummy module",
+    name="Python Template",
     version="0.0.1",
     author="Axel Mendoza",
     author_email="axel.mendoza@epita.fr",
     description="Example description",
     license="BSD",
-    packages=['mymodule'],
+    packages=['template_package'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
